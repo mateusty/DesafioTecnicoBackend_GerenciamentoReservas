76499356b2df1f6cf85a4404fcc50506efd12d84
mateusty <mateustamaki43@gmail.com>
2026-08-16 15:17:29 -0300

    feat: Adding valid email validation

--- src/Presentation/Identity/LoginRequest.cs
+++ src/Presentation/Identity/LoginRequest.cs
@@ -1,11 +1,14 @@
-using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace Presentation.Identity;
 
 public class LoginRequest
 {
 
+    [Required(ErrorMessage = "Email é obrigatório")]
+    [EmailAddress(ErrorMessage = "Informe um email válido")]
     public string Email { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Senha é obrigatória")]
     public string Password { get; set; } = string.Empty;
 }
