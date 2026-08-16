d2ec2055b635a8ad370a4ac4cb7fc42bc6ead607
mateusty <mateustamaki43@gmail.com>
2026-08-16 16:34:09 -0300

    feat: Adding RabbitMQ integration test

---
+++ IntegrationTests/RabbitMQIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+using RabbitMQ.Client;
+
+namespace IntegrationTests
+{
+    public class RabbitMQIntegrationTests
+    {
+        [Fact]
+        public async Task RabbitMQ_SendingMessage()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/"
+            };
+
+            await using var connection = await factory.CreateConnectionAsync();
+            await using var channel = await connection.CreateChannelAsync();
+
+            string queue = "queueTest";
+
+            await channel.QueueDeclareAsync(
+                queue: queue,
+                durable: false,
+                exclusive: true,
+                autoDelete: true
+                );
+
+            var message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync("", queue, body);
+
+            BasicGetResult? result = await channel.BasicGetAsync(queue, true);
+            Assert.NotNull(result);
+            var queueMessage = Encoding.UTF8.GetString(result!.Body.ToArray());
+
+            Assert.Equal(message, queueMessage);
+        }
+    }
+}
